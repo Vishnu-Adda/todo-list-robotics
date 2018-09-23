8f273d6f1249de19b1671b18a0793d37fd93781a
Vishnu Adda <addavishnu@gmail.com>
2018-09-23 15:34:57 -0700

    Initial commit.

---
+++ classes.js
@@ -0,0 +1,11 @@
+class Todo {
+    constructor(text) {
+        this.text = text;
+    }
+    get text() {
+        return this.text;
+    }
+    set text(editedText) {
+        this.text = editedText;
+    }
+};
